5091faf2fa2c0a9391ca2dde5b6ba06892495e75
DavidPagliano <paglianodavid@gmail.com>
2020-10-12 23:32:47 -0300

    separation of header, main and footer complete

--- src/components/screens/home/hearder/index.js
+++ src/components/screens/home/hearder/index.js
@@ -2,22 +2,24 @@ import './index.css'
 import React from 'react'
 import Home from '../index'
 import Form from '../../form'
+import Counter from '../../counter'
 import {BrowserRouter, Route,Switch, Link} from 'react-router-dom'
 function header (){
   
   return(
-  <BrowserRouter>
-    <div className="header">
-      <ul>
-        <li Link to='home'>Home <Home/></li>
-        <li Link to='form'>Formulario <Form/></li>
-        <li>Contacto</li>
-      </ul>
-    </div>
-    <Route path='home'/>
-    <Route path='form'/>
-  </BrowserRouter>
-   
+    
+   <BrowserRouter>
+      <div className='header'>
+          <ul>
+            <li><Link to='home'>Home</Link></li>
+            <li><Link to='form'>Formulario</Link></li>
+            <li><Link to='counter'>Counter</Link></li>
+          </ul>
+      </div>
+      <Route path='home'><Home/></Route>
+      <Route path='form'><Form/></Route>
+      <Route path='counter'><Counter/></Route>
+   </BrowserRouter>
      
   )
   
